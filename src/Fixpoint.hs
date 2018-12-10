2b5a4ba747b87169a4151d19c3ee2f0fe589e8dd
Francisco Gutierrez <freefrancisco@gmail.com>
2018-12-09 16:42:30 -0800

    two different definitions of fixpoint, test them both with the same hspec code

--- src/Fixpoint.hs
+++ src/Fixpoint.hs
@@ -1,6 +1,10 @@
 module Fixpoint
     ( fixpoint
+    , fixpoint'
     ) where
 
 fixpoint :: (a -> a) -> a
-fixpoint f = f (fixpoint f)
+fixpoint f = fp where fp = f fp
+
+fixpoint' :: (a -> a) -> a
+fixpoint' f = f (fixpoint' f)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,34 @@
 import Test.Hspec
 import Test.QuickCheck
 
-import Fixpoint
+import Fixpoint (fixpoint, fixpoint')
 
 main :: IO ()
 main = hspec $ do
-    describe "fixpoint of a constant" $ do
-        it "the fixpoint of a constant function is that constant" $ do
-            fixpoint (\x -> 3) `shouldBe` 3
-        it "the fixpoint of any constant is that constant" $ do
-            property $ \n -> fixpoint (\x -> n) == (n :: Int)
+    desc_constant fixpoint
+    desc_factorial fixpoint
+    desc_fibonacci fixpoint
+    desc_constant fixpoint'
+    desc_factorial fixpoint'
+    desc_fibonacci fixpoint'
 
-    describe "fixpoint in recursion" $ do
-        it "the fixpoint of fac' f n = n * f (n -1 ) is the factorial" $ do
-            let fac' f 0 = 1
-                fac' f n = n * f (n - 1)
-                factorial n = product [1..n]
-            property $ \n -> (n :: Int) > 0 ==> fixpoint fac' n == factorial n
+desc_constant fixp = describe "fixpoint of a constant function" $ do
+    it "the fixpoint of a constant function is that constant" $ do
+        fixp (\x -> 3) `shouldBe` 3
+        fixp (const 2) `shouldBe` 2
+    it "the fixpoint of any constant is that constant" $ do
+        property $ \n -> fixp (\x -> n) == (n :: Int)
+
+desc_factorial fixp = describe "fixpoint in recursion" $ do
+    let fac n = product [1..n]
+    it "the fixpoint of \\f n = n * f (n - 1) is the factorial" $ do
+        let f' f 0 = 1
+            f' f n = n * f (n - 1)
+            fac' = fixp f'
+        property $ \n -> (n :: Int) > 0 ==> fac' n == fac n
+
+desc_fibonacci fixp = describe "fixpoint fibonacci" $ do
+    let fibs = scanl (+) 0 (1:fibs)
+    it "the fixpoint of a fibonacci sequence" $ do
+        let fibs' = fixp (scanl (+) 0 . (1:))
+        property $ \n -> (n :: Int) > 0 ==> (fibs !! n) == (fibs' !! n)
